916d171349695590995e486974a9b01c6d277553
Rishant <rishantsingh0707@gmail.com>
2026-06-30 19:15:20 +0530

    Added Streaming Chat feature so user dosnt have to wait for res. it will render it in small chunks.

--- backend/src/services/chatService.js
+++ backend/src/services/chatService.js
@@ -7,7 +7,7 @@ const groq = new Groq({
     apiKey: process.env.GROQ_API_KEY,
 });
 
-export const generateAnswer = async (question, chunks,history) => {
+export const generateAnswer = async (question, chunks, history) => {
 
     const context =
         chunks.join("\n\n");
@@ -28,9 +28,44 @@ export const generateAnswer = async (question, chunks,history) => {
                     content: `Context:${context}Question:${question}`,
                 },
             ],
+            stream: true
         });
 
     return completion
         .choices[0]
         .message.content;
 };
+
+export const streamAnswer = async (question, chunks, history) => {
+
+    const context = chunks.join("\n\n");
+
+    return await groq.chat.completions.create({
+        model: "llama-3.3-70b-versatile",
+
+        stream: true,
+
+        messages: [
+            {
+                role: "system",
+                content: `You are an AI Study Assistant.
+
+Answer ONLY from the provided context.
+
+If the answer is not available in the context, clearly say so.
+
+Context:
+
+${context}`,
+            },
+
+            ...history,
+
+            {
+                role: "user",
+                content: question,
+            },
+        ],
+    });
+};
+
